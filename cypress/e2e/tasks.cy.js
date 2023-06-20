d553b822046709ea620cf108b6c793dc6db45d32
Rubendvb <rubenvasquez556@gmail.com>
2023-06-20 17:52:37 -0300

    usando api faker para gerar massa de dados dinâmicos

--- cypress/e2e/tasks.cy.js
+++ cypress/e2e/tasks.cy.js
@@ -1,10 +1,12 @@
 /// <reference types="cypress" />
 
+import { faker } from "@faker-js/faker";
+
 describe("tasks", () => {
   it("deve cadastrar uma nova tarefa", () => {
     cy.visit("http://127.0.0.1:8080/");
 
-    cy.get("#newTask").type("Ler um livro de node.js");
+    cy.get("#newTask").type(faker.lorem.words(5));
 
     // button[contains(text(), "Create")]
     cy.contains("button", "Create").click();
